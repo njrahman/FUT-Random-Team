a66904dc13c178a278b042883f4dadafbb73c082
njrahman <njrahman@email.wm.edu>
2019-01-27 14:00:18 -0500

    Switched dataset, random team generator(no chemistry)

--- import_data.py
+++ import_data.py
@@ -2,10 +2,12 @@ import pandas as pd
 import pprint as pprint
 import random as random
 
-player_data = pd.read_csv("fifa_player_data.csv")
 
-players = player_data["Name"]
+player_data = pd.read_csv("fifa_19_player_data.csv", encoding="latin-1")
+
+players = player_data["Player Name"]
 clubs = player_data["Club"]
+leagues = player_data["League"]
 nationalities = player_data["Nationality"]
 positions = player_data["Position"]
 player_info = {}
@@ -13,11 +15,21 @@ player_info = {}
 i = 0
 
 while i < players.size:
-   player_info[players[i]] = [clubs[i],nationalities[i],positions[i]]
+   player_info[players[i]] = [clubs[i],nationalities[i],leagues[i],positions[i]]
    i+=1
 
-#print(player_info.items())
+x = random.randint(1,players.size)
+player = players[x]
 
-print(random.randint(1,players.size))
-
-#pprint.pprint(player_data)
+formation_442 = ["GK","RB","CB","CB","LB","RM","CM","CM","LM","ST","ST"]
+team = {}
+for position in formation_442:
+   print(position)
+   x = random.randint(1,players.size)
+   player = players[x]
+   while player_info[player][3] != position:
+      x = random.randint(1,players.size)
+      player = players[x]
+   team[player] = player_info[player] 
+   print(player_info[player])
+pprint.pprint(team)
